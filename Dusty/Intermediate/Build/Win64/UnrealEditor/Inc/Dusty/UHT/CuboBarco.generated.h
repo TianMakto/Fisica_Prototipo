f6173d16f94a3acdffb4852ed7a8bebfbaa937d4
Ounsesa <carlos.rocal01@gmail.com>
2024-03-04 16:54:08 +0100

    camara y cosas

---
+++ Dusty/Intermediate/Build/Win64/UnrealEditor/Inc/Dusty/UHT/CuboBarco.generated.h
@@ -0,0 +1,108 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "CuboBarco.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef DUSTY_CuboBarco_generated_h
+#error "CuboBarco.generated.h already included, missing '#pragma once' in CuboBarco.h"
+#endif
+#define DUSTY_CuboBarco_generated_h
+
+#define FID_Users_carlos_calzada_Documents_GitHub_Fisica_Prototipo_Dusty_Source_Dusty_CuboBarco_h_12_SPARSE_DATA
+#define FID_Users_carlos_calzada_Documents_GitHub_Fisica_Prototipo_Dusty_Source_Dusty_CuboBarco_h_12_RPC_WRAPPERS \
+ \
+	DECLARE_FUNCTION(execTurn); \
+	DECLARE_FUNCTION(execDeccelerate); \
+	DECLARE_FUNCTION(execAccelerate);
+
+
+#define FID_Users_carlos_calzada_Documents_GitHub_Fisica_Prototipo_Dusty_Source_Dusty_CuboBarco_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+ \
+	DECLARE_FUNCTION(execTurn); \
+	DECLARE_FUNCTION(execDeccelerate); \
+	DECLARE_FUNCTION(execAccelerate);
+
+
+#define FID_Users_carlos_calzada_Documents_GitHub_Fisica_Prototipo_Dusty_Source_Dusty_CuboBarco_h_12_ACCESSORS
+#define FID_Users_carlos_calzada_Documents_GitHub_Fisica_Prototipo_Dusty_Source_Dusty_CuboBarco_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesACuboBarco(); \
+	friend struct Z_Construct_UClass_ACuboBarco_Statics; \
+public: \
+	DECLARE_CLASS(ACuboBarco, APawn, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/Dusty"), NO_API) \
+	DECLARE_SERIALIZER(ACuboBarco)
+
+
+#define FID_Users_carlos_calzada_Documents_GitHub_Fisica_Prototipo_Dusty_Source_Dusty_CuboBarco_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesACuboBarco(); \
+	friend struct Z_Construct_UClass_ACuboBarco_Statics; \
+public: \
+	DECLARE_CLASS(ACuboBarco, APawn, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/Dusty"), NO_API) \
+	DECLARE_SERIALIZER(ACuboBarco)
+
+
+#define FID_Users_carlos_calzada_Documents_GitHub_Fisica_Prototipo_Dusty_Source_Dusty_CuboBarco_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ACuboBarco(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ACuboBarco) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ACuboBarco); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACuboBarco); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ACuboBarco(ACuboBarco&&); \
+	NO_API ACuboBarco(const ACuboBarco&); \
+public: \
+	NO_API virtual ~ACuboBarco();
+
+
+#define FID_Users_carlos_calzada_Documents_GitHub_Fisica_Prototipo_Dusty_Source_Dusty_CuboBarco_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ACuboBarco(ACuboBarco&&); \
+	NO_API ACuboBarco(const ACuboBarco&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ACuboBarco); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACuboBarco); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ACuboBarco) \
+	NO_API virtual ~ACuboBarco();
+
+
+#define FID_Users_carlos_calzada_Documents_GitHub_Fisica_Prototipo_Dusty_Source_Dusty_CuboBarco_h_9_PROLOG
+#define FID_Users_carlos_calzada_Documents_GitHub_Fisica_Prototipo_Dusty_Source_Dusty_CuboBarco_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Users_carlos_calzada_Documents_GitHub_Fisica_Prototipo_Dusty_Source_Dusty_CuboBarco_h_12_SPARSE_DATA \
+	FID_Users_carlos_calzada_Documents_GitHub_Fisica_Prototipo_Dusty_Source_Dusty_CuboBarco_h_12_RPC_WRAPPERS \
+	FID_Users_carlos_calzada_Documents_GitHub_Fisica_Prototipo_Dusty_Source_Dusty_CuboBarco_h_12_ACCESSORS \
+	FID_Users_carlos_calzada_Documents_GitHub_Fisica_Prototipo_Dusty_Source_Dusty_CuboBarco_h_12_INCLASS \
+	FID_Users_carlos_calzada_Documents_GitHub_Fisica_Prototipo_Dusty_Source_Dusty_CuboBarco_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FID_Users_carlos_calzada_Documents_GitHub_Fisica_Prototipo_Dusty_Source_Dusty_CuboBarco_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Users_carlos_calzada_Documents_GitHub_Fisica_Prototipo_Dusty_Source_Dusty_CuboBarco_h_12_SPARSE_DATA \
+	FID_Users_carlos_calzada_Documents_GitHub_Fisica_Prototipo_Dusty_Source_Dusty_CuboBarco_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_Users_carlos_calzada_Documents_GitHub_Fisica_Prototipo_Dusty_Source_Dusty_CuboBarco_h_12_ACCESSORS \
+	FID_Users_carlos_calzada_Documents_GitHub_Fisica_Prototipo_Dusty_Source_Dusty_CuboBarco_h_12_INCLASS_NO_PURE_DECLS \
+	FID_Users_carlos_calzada_Documents_GitHub_Fisica_Prototipo_Dusty_Source_Dusty_CuboBarco_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> DUSTY_API UClass* StaticClass<class ACuboBarco>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Users_carlos_calzada_Documents_GitHub_Fisica_Prototipo_Dusty_Source_Dusty_CuboBarco_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Dusty/Intermediate/Build/Win64/x64/DustyEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.InclOrderUnreal5_0.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.34.31942
+#include "C:/Users/carlos.calzada/Documents/GitHub/Fisica_Prototipo/Dusty/Intermediate/Build/Win64/x64/DustyEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.InclOrderUnreal5_0.h"
